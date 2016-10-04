9e57a1381f029d99d9bfed4b0962ac2a5cc19f89
GILBZ <benzvi.gil@gmail.com>
2016-10-04 20:03:00 +0300

    Spinner control implementation added (without glue logic)

---
+++ Elicon.Wpf.UI/Controls/AttachedAdorner.cs
@@ -0,0 +1,127 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace EdaTools.Controls
+{
+
+    public class AttachedAdorner : Adorner
+    {
+
+        private readonly FrameworkElement _owner;
+        private readonly FrameworkElement _element;
+
+        public AttachedAdorner(FrameworkElement owner, FrameworkElement element)
+            : base(owner)
+        {
+            _owner = owner;
+            _element = element;
+            _element.Focusable = false;
+            AddVisualChild(_element);
+        }
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            _element.Measure(constraint);
+            return _element.DesiredSize;
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            _element?.Arrange(new Rect(new Point(0, 0), _owner.DesiredSize));
+            return finalSize;
+        }
+
+        protected override Visual GetVisualChild(int index)
+        {
+            return _element;
+        }
+
+        protected override int VisualChildrenCount => 1;
+
+        #region Adorner attached property.
+        public static readonly DependencyProperty AdornerProperty =
+            DependencyProperty.RegisterAttached("Adorner", typeof(FrameworkElement), typeof(AttachedAdorner),
+            new FrameworkPropertyMetadata(default(Spinner), PropertyChangedCallback));
+
+        public static void SetAdorner(DependencyObject element, FrameworkElement value)
+        {
+            element.SetValue(AdornerProperty, value);
+        }
+
+        public static FrameworkElement GetAdorner(DependencyObject element)
+        {
+            return (FrameworkElement)element.GetValue(AdornerProperty);
+        }
+
+        private static void PropertyChangedCallback(DependencyObject dependencyObject,
+            DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var frameworkElement = dependencyObject as FrameworkElement;
+            if (frameworkElement != null)
+            {
+                frameworkElement.Loaded += AdornerLoaded;
+            }
+        }
+
+        private static void AdornerLoaded(object sender, RoutedEventArgs e)
+        {
+            var frameworkElement = sender as FrameworkElement;
+            if (frameworkElement != null)
+            {
+                var adornerLayer = AdornerLayer.GetAdornerLayer(frameworkElement);
+                if (adornerLayer != null)
+                {
+                    // If GetSpinner() returns a type.
+                    //x var spinner = (Spinner) Activator.CreateInstance(GetSpinner(frameworkElement)); //, frameworkElement);
+                    var adorner = GetAdorner(frameworkElement);
+                    SetAdorningElement(frameworkElement, adorner);
+                    var attachedAdorner = new AttachedAdorner(frameworkElement, adorner);
+                    adornerLayer.Add(attachedAdorner);
+                 //x   adorner.Visibility = Visibility.Visible;
+                }
+            }
+        }
+        #endregion
+
+        #region AdornerVisible attached property
+        public static readonly DependencyProperty AdornerVisibleProperty = 
+            DependencyProperty.RegisterAttached("AdornerVisible", typeof(bool), typeof(AttachedAdorner),
+            new PropertyMetadata(false, AdornerVisibilityChanged));
+        public static bool GetAdornerVisible(FrameworkElement target)
+        {
+            return (bool)target.GetValue(AdornerVisibleProperty);
+        }
+        public static void SetAdornerVisible(FrameworkElement target, bool value)
+        {
+            target.SetValue(AdornerVisibleProperty, value);
+        }
+        private static void AdornerVisibilityChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var adorned = (FrameworkElement)d;
+            var adorner = GetAdorningElement(adorned);
+            if ((bool)e.NewValue)
+                adorner.Visibility = Visibility.Visible;
+            else
+                adorner.Visibility = Visibility.Hidden;
+            //x ((Spinner)adorner).SetVisible((bool)e.NewValue);
+
+        }
+        #endregion
+
+        #region AdorningElement attached property
+        public static readonly DependencyProperty AdorningElementProperty =
+            DependencyProperty.RegisterAttached("AdorningElement", typeof(FrameworkElement), typeof(AttachedAdorner));
+
+        public static FrameworkElement GetAdorningElement(DependencyObject target)
+        {
+            return (FrameworkElement)target.GetValue(AdorningElementProperty);
+        }
+        public static void SetAdorningElement(DependencyObject target, FrameworkElement value)
+        {
+            target.SetValue(AdorningElementProperty, value);
+        }
+        #endregion
+    }
+
+}
---
+++ Elicon.Wpf.UI/Controls/Spinner.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace EdaTools.Controls
+{
+    public partial class Spinner
+    {
+        private readonly DispatcherTimer _spinnerTimer;
+        private readonly Ellipse[] _circles;
+        private readonly RadialGradientBrush _brush;
+
+        public Spinner()
+        {
+            InitializeComponent();
+            IsVisibleChanged += HandleVisibleChanged;
+            _spinnerTimer = new DispatcherTimer(DispatcherPriority.Normal, Dispatcher) { Interval = new TimeSpan(0, 0, 0, 0, 100) };
+            _circles = new Ellipse[9];
+            _brush = new RadialGradientBrush
+                {
+                    GradientOrigin = new Point(0.5, 0.5),
+                    Center = new Point(0.5, 0.5),
+                    RadiusX = 0.5,
+                    RadiusY = 0.5,
+                    GradientStops = new GradientStopCollection()
+                };
+            _brush.GradientStops.Add(new GradientStop(Colors.Transparent, 0.0));
+            _brush.GradientStops.Add(new GradientStop(Color.FromRgb(90, 90, 90), 1.0));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            CreateSpinnerCircles();
+        }
+
+        private void HandleUnloaded(object sender, RoutedEventArgs e)
+        {
+            Stop();
+        }
+
+        private void CreateSpinnerCircles()
+        {
+            for (var i = 0; i < 9; i++)
+            {
+                _circles[i] = new Ellipse
+                    {
+                        Stretch = Stretch.Fill,
+                        Width = 20,
+                        Height = 20,
+                        Fill = _brush,
+                        Opacity = 1 - i * 0.11
+                    };
+                Canvas1.Children.Add(_circles[i]);
+                SetCirclePosition(_circles[i], i);
+            }
+        }
+
+        public void SetVisible(bool visible)
+        {
+            if (visible)
+                Start();
+            else
+                Stop();
+        }
+
+        private void Start()
+        {
+            _spinnerTimer.Tick += OnSpinnerTick;
+            _spinnerTimer.Start();
+            SpinnerRotate.Angle = 0;
+            Opacity = 1;
+        }
+
+        private void Stop()
+        {
+            _spinnerTimer.Stop();
+            _spinnerTimer.Tick -= OnSpinnerTick;
+            Opacity = 0;
+        }
+
+        private void OnSpinnerTick(object sender, EventArgs e)
+        {
+            SpinnerRotate.Angle = (SpinnerRotate.Angle + 36) % 360;
+        }
+
+        private static void SetCirclePosition(DependencyObject ellipse, double sequence)
+        {
+            ellipse.SetValue(Canvas.LeftProperty, 50.0 + (Math.Sin(Math.PI * ((0.2 * sequence) + 1)) * 50.0));
+            ellipse.SetValue(Canvas.TopProperty, 50.0 + (Math.Cos(Math.PI * ((0.2 * sequence) + 1)) * 50.0));
+        }
+
+        public void HandleVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (System.ComponentModel.DesignerProperties.GetIsInDesignMode(this) == false)
+            {
+                var isVisible = (bool)e.NewValue;
+                if (isVisible)
+                    Start();
+                else
+                    Stop();
+            }
+        }
+
+    }
+}
