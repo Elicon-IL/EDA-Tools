0ebf87e78c02a17d0498e17351833d2cf2572b2a
Udi Ben Zvi <flytoeilat@gmail.com>
2016-10-10 15:49:17 +0300

    remove using

--- Elicon.Domain/GateLevel/Manipulations/Interceptors/ManipulationExceptionInterceptor.cs
+++ Elicon.Domain/GateLevel/Manipulations/Interceptors/ManipulationExceptionInterceptor.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Linq;
 using Elicon.Domain.GateLevel.Contracts.DataAccess;
-using Elicon.Domain.GateLevel.Manipulations.RemoveBuffer;
 using Ninject.Extensions.Interception;
 
 namespace Elicon.Domain.GateLevel.Manipulations.Interceptors
